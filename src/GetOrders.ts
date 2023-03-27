05e255e9cd2b03c34c890806aa93937d5e1a200f
moji94 <mojtababahadori94@gmail.com>
2023-03-27 17:12:10 +0430

    Update GetOrders

--- src/GetOrders.ts
+++ src/GetOrders.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { Request, Response } from 'express'
+
 const prisma = new PrismaClient()
 
 export const GetOrders = async (req: Request, res: Response): Promise<void> => {
