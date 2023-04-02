328b2e61c1204e179b7d18983fe5c77979b71ecb
moji94 <mojtababahadori94@gmail.com>
2023-04-02 19:02:33 +0430

    Update CreateAdmin

--- src/CreateAdmin.ts
+++ src/CreateAdmin.ts
@@ -9,26 +9,35 @@ export const CreateAdmin = async (
 ): Promise<void> => {
   try {
     const name: string = req.body.name
-    const phone: number = req.body.phone
+    const phone: number = Number(req.body.phone)
     const email: string = req.body.email
     const password: string = req.body.password
     console.log(req.body)
-    const create = await prisma.admins.create({
-      data: {
-        id: undefined,
-        name: name,
+    const check = await prisma.admins.findMany({
+      where: {
         phone: phone,
-        email: email,
-        password: password,
-        token: 'empty',
-        created: new Date(),
-        updated: new Date(),
       },
     })
-    if (create) {
-      res.status(200).json('done')
+    if (check.length === 0) {
+      const create = await prisma.admins.create({
+        data: {
+          id: undefined,
+          name: name,
+          phone: phone,
+          email: email,
+          password: password,
+          token: 'empty',
+          created: new Date(),
+          updated: new Date(),
+        },
+      })
+      if (create) {
+        res.status(200).json('done')
+      } else {
+        res.status(400).json('data problem')
+      }
     } else {
-      res.status(400).json('data problem')
+      res.status(400).json('exist')
     }
   } catch (e) {
     res.status(500).json(e)
