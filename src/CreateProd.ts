e6071e0517b4dd725432824b6123c7dc3d577195
moji94 <mojtababahadori94@gmail.com>
2023-04-03 06:54:26 +0430

    Update CreateProd

--- src/CreateProd.ts
+++ src/CreateProd.ts
@@ -7,36 +7,38 @@ export const CreateProd = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  try {
-    const name: string = req.body.name
-    const comment: string = req.body.comment
-    const perc: number = req.body.perc
-    const subcategoriesID: string = req.body.subcatId
-    const mainSl: number = req.body.mainSl
-    const mostSell: number = req.body.mostSell
-    const wonder: number = req.body.wonder
-    const create = await prisma.products.create({
-      data: {
-        id: undefined,
-        name: name,
-        comment: comment,
-        perc: perc,
-        subcategoriesId: subcategoriesID,
-        mainSl: mainSl,
-        mostSell: mostSell,
-        wonder: wonder,
-        status: 'available',
-        sold: 0,
-        created: new Date(),
-        updated: new Date(),
-      },
-    })
-    if (create) {
-      res.status(200).json('done')
-    } else {
-      res.status(400).json('problem')
-    }
-  } catch (e) {
-    res.status(500).json(e)
+  console.log(req.body)
+  // try {
+  const name: string = req.body.formData.name
+  const comment: string = req.body.formData.comment
+  const perc: number = Number(req.body.formData.perc)
+  const subcategoriesID: string = req.body.formData.subcatId
+
+  const mainSl: number = req.body.formData.mainSl
+  const mostSell: number = req.body.formData.mostSell
+  const wonder: number = req.body.formData.wonder
+  const create = await prisma.products.create({
+    data: {
+      id: undefined,
+      name: name,
+      comment: comment,
+      perc: perc,
+      subcategoriesId: subcategoriesID,
+      mainSl: mainSl,
+      mostSell: mostSell,
+      wonder: wonder,
+      status: 'available',
+      sold: 0,
+      created: new Date(),
+      updated: new Date(),
+    },
+  })
+  if (create) {
+    res.status(200).json('done')
+  } else {
+    res.status(400).json('problem')
   }
+  // } catch (e) {
+  //   res.status(500).json(e)
+  // }
 }
